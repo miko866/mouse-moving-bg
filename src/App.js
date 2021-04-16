f58890df440f1b58eadd9bfdfce938e4442c80f0
miko866 <m.durik@substring.ch>
2021-04-16 22:27:57 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useCallback } from 'react'
+
+const width = 960
+const height = 500
+const circleRadius = 30
+const initialMousePosition = { x: width / 2, y: height / 2 }
 
 function App() {
+  const [mousePosition, setMousePosition] = useState(initialMousePosition)
+
+  const handleMouseMove = useCallback(
+    (event) => {
+      const { clientX, clientY } = event
+      setMousePosition({ x: clientX, y: clientY })
+    },
+    [setMousePosition]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <svg width={width} height={height} onMouseMove={handleMouseMove}>
+      <circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} />
+    </svg>
+  )
 }
 
-export default App;
+export default App
